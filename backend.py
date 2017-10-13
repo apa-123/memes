066a47eeae37cfefbe748af6a1cb7565869722d1
Thomas Yang <thomasy4@illinois.edu>
2017-10-12 21:10:37 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,17 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 class BackEnd:
 	def __init__(self):
+		return
 		
 	def displayText(self, message):
 		return message
 	
 	def displayImage(self, imageLink):
 		return imageLink
+
+
+test = BackEnd()
+
 @app.route('/')
 def index():
-	return "asdf"
+	return test.displayText("asdfasdf")
 
 if __name__ == '__main__':
 	app.run()
