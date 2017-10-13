81da0d442131dc30e53f7ed8c50755183bb7fd39
Thomas Yang <thomasy4@illinois.edu>
2017-10-12 21:03:55 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,12 +1,18 @@
-
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-
+class BackEnd:
+	def __init__(self):
+		
+	def displayText(self, message):
+		return message
+	
+	def displayImage(self, imageLink):
+		return imageLink
 @app.route('/')
 def index():
-	return render_template('home.html')
+	return "asdf"
 
 if __name__ == '__main__':
 	app.run()
