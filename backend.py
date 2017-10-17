0617938d59652c83a09ef3b5bc0310dea1236f50
Aditya Bhansali <adityaneal@hotmail.com>
2017-10-16 19:18:48 -0500

    removed BackEnd class

--- backend.py
+++ backend.py
@@ -2,28 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-class BackEnd:
-	def __init__(self):
-		return
-		
-	def displayText(self, message):
-		# User inputs string parameter which is 
-		# displayed on the webpage
-		return message
+# helper functions	
+def displayText(message):
+	# User inputs string parameter which is 
+	# displayed on the webpage
+	return message
 	
-	def displayImage(self, name):
-		# User inputs the image Link which is 
-		# displayed on the webpage
-		# WIP
-		return render_template('home.html', name=name)
+def displayImage(name):
+	# User inputs the image Link which is 
+	# displayed on the webpage
+	# WIP
+	return render_template('home.html', name=name)
 
-# creates instance of the class BackEnd file
-test = BackEnd()
-
+# home page
 @app.route('/')
 def index():
-	return test.displayImage("https://www.w3schools.com/w3images/fjords.jpg")
+	return displayImage("https://www.w3schools.com/w3images/fjords.jpg")
 
 if __name__ == '__main__':
 	app.run()
