4169a1a1b2843651a8c14b1516450c2a36fe1227
Thomas Yang <thomasy4@illinois.edu>
2017-10-30 20:10:54 -0500

    created user class and editted json file

--- userClass.py
+++ userClass.py
@@ -1,7 +1,42 @@
+import json
+from pprint import pprint
+
 class User:
-	"""A user object. 
 
-	"""
+	def __init__(self, username):
+		json_data=open("dummy_user.json").read()
+		data = json.loads(json_data)
 
-	def __init__(self, name):
-		
+		self.username = username
+		self.first_name = data[username]["first_name"]
+		self.second_name = data[username]["second_name"]
+		self.picture = data[username]["picture"]
+		self.age = data[username]["age"]
+		self.education = data[username]["education"]
+		self.geography = data[username]["geography"]
+		self.subreddit = data[username]["subreddit"]
+		self.meme = data[username]["meme"]
+
+	def returnFirstName(self):
+		return "username = " + self.username + "\nfirst name = " + self.first_name
+
+	def returnSecondName(self):
+		return "username = " + self.username + "\nsecond name = " + self.second_name
+
+	def returnPicture(self):
+		return "username = " + self.username + "\npicture = " + self.picture
+
+	def returnAge(self):
+		return "username = " + self.username + "\nAge = " + self.age
+
+	def returnEducation(self):
+		return "username = " + self.username + "\nEducation = " + self.education
+
+	def returnGeography(self):
+		return "username = " + self.username + "\nGeography = " + self.geography
+
+	def returnSubreddit(self):
+		return "username = " + self.username + "\nSubreddit = " + self.subreddit
+
+	def returnMeme(self):
+		return "username = " + self.username + "\nMeme = " + self.meme
