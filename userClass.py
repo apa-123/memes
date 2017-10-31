4e95e4b9837357dd4e1322c0f10dfc3b43fe59f6
Thomas Yang <thomasy4@illinois.edu>
2017-10-30 20:43:36 -0500

    added some comments

--- userClass.py
+++ userClass.py
@@ -1,21 +1,39 @@
 import json
-from pprint import pprint
 
 class User:
+	"""Class containing information about the user.
+	
+	Attributes:
+	username: String that represents the username
+	password: String that represents the password
+	is_valid: Boolean that is true when the password is correct for given username
+	"""
 
-	def __init__(self, username):
+	def __init__(self, username, password):
 		json_data=open("dummy_user.json").read()
 		data = json.loads(json_data)
-
-		self.username = username
-		self.first_name = data[username]["first_name"]
-		self.second_name = data[username]["second_name"]
-		self.picture = data[username]["picture"]
-		self.age = data[username]["age"]
-		self.education = data[username]["education"]
-		self.geography = data[username]["geography"]
-		self.subreddit = data[username]["subreddit"]
-		self.meme = data[username]["meme"]
+		if data[username]["password"] == password:
+			self.username = username
+			self.first_name = data[username]["first_name"]
+			self.second_name = data[username]["second_name"]
+			self.picture = data[username]["picture"]
+			self.age = data[username]["age"]
+			self.education = data[username]["education"]
+			self.geography = data[username]["geography"]
+			self.subreddit = data[username]["subreddit"]
+			self.meme = data[username]["meme"]
+			self.is_valid = True
+		else:
+			self.username = username
+			self.first_name = ""
+			self.second_name = ""
+			self.picture = ""
+			self.age = ""
+			self.education = ""
+			self.geography = ""
+			self.subreddit = ""
+			self.meme = ""
+			self.is_valid = False
 
 	def __str__(self):
 		return "Username = " + self.username + "\nFirst Name = " + self.first_name + "\nSecond Name = " + self.second_name + "\nPicturelink = " + self.picture + "\nAge = " + self.age + "\nEducation = " + self.education + "\nGeography = " + self.geography + "\nSubreddit = " + self.subreddit + "\nMeme = " + self.meme
@@ -43,4 +61,3 @@ class User:
 
 	def returnMeme(self):
 		return "username = " + self.username + "\nMeme = " + self.meme
-
