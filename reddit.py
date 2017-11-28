787eb0103f4e12d590ee822e299fcb6ea346a46a
yaba12 <31941014+yaba12@users.noreply.github.com>
2017-11-27 20:28:49 -0600

    Update reddit.py
    @yaba12
    Commit changes

--- reddit.py
+++ reddit.py
@@ -1,63 +1,45 @@
 import praw 
-from pprint import pprint
-
-import requests
-
-import json#reddit wrapper, helps parse json. Short for python reddit api wrapper. Must install it to run it.  
-
-#following is trying to get reddit json. To get a short description of the subreddit, go to reddit.com/reddits. Then get the json of /reddits and parse it to find the "public description" and get the string afterward to get the short description. Haven't found any other way of finding short description. Haven't done so yet.
-'''
-#import requests
-#r = requests.get(
-    'https://reddit.com/reddits.json',
-    headers={'User-Agent': ''}
-)
-r.text # Unformatted text
-reddit_json = json.loads(r.text)
-reddit_json
-'''
+# Python Reddit API Wrapper. https://praw.readthedocs.io/en/latest/. Praw allows us to access Reddit API and helps us parse the data from the Reddit API.
 class Reddit:
 
 	def __init__(self, nameOfSubreddit, limits):
+		#Initializies the name of the subreddit.
 		self.nameOfSubreddit = nameOfSubreddit
+		#Initializies the amount of posts you retrieve from reddit.
 		self.limits = limits
-		self.reddit = praw.Reddit(client_id='kCrm1Fz7g6alMw',
+		#Initializies an instance of Reddit. Need account and application information to access it.
+		self.praw = praw.Reddit(client_id='kCrm1Fz7g6alMw',
 	        	client_secret='z3l8kIhB9zYAQl7QUobTOkBGCnQ',
 	                user_agent='testscriptplswork',
 	                username='joshdunigan',
 	                password='joshdunigan123')
-		self.subreddit = self.reddit.subreddit(nameOfSubreddit)
+	    #Initializes the subreddit. 
+		self.subreddit = self.praw.subreddit(nameOfSubreddit)
+		#Initializies a list used to return the information.
 		self.returnList = []
-		self.iteratethis = self.subreddit.top(time_filter = 'day', limit = limits)
-
+		#Gets the limits number of posts from the "top" section of the subreddit.
+		self.posts = self.subreddit.top(time_filter = 'day', limit = limits)
+	#Return the name of the Subreddit.
 	def getSubredditName(self):
 		return self.nameOfSubreddit
-
+	#Changes the subreddit name.
+	#@param Name of the subreddit you want to change to.
 	def setSubreddit(self, subredditName):
 		self.nameOfSubreddit = subredditName
-	
-	#following functions only print from hot and of the top 10. Can change it by changing the "hot" and the limit to a higher or lower number. All require a String parameter that state the subreddit you want.
-
-	#get the image link for the top 10 in hot section. Returns list filled with url. Param limit 
-
+		self.subreddit = self.praw.subreddit(nameOfSubreddit)
+	#Returns a list full of Urls of the 
 	def getImageUrl(self):
-		for submission in self.iteratethis:
-	   		self.returnList.append(submission.url)
+		self.returnList = [submission.url for submission in self.posts]
 		return self.returnList
-
-	#gets the title for the top 10 in hot section. Returns list of Titles.
+	#Returns a list of titles of the posts given the 
 	def getTitle(self):
-		for submission in self.iteratethis:
-	   		self.returnList.append(submission.title)
+		self.returnList = [submission.title for submission in self.posts]
 		return self.returnList
-
-	#gets the score. Returns list of scores.
+	#Returns a list of ints of scores of the posts.
 	def getScore(self):
-		for submission in self.iteratethis:
-	   		self.returnList.append(submission.score)
+		self.returnList = [submission.score for submission in self.posts]
 		return self.returnList
-
+	#Returns a list of the usernames who submitted the posts.
 	def getAuthor(self):
-		for submission in self.iteratethis:
-			self.returnList.append(submission.author)
+		self.returnList = [submission.author for submission in self.posts]
 		return self.returnList
