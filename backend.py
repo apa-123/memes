6cb66b7fe768bea6139cf5540bb4fc5fe20e9a6a
Aditya Bhansali <adityaneal@hotmail.com>
2017-11-27 20:04:38 -0600

    added docstrings

--- backend.py
+++ backend.py
@@ -10,55 +10,95 @@ from categoryClass import Category
 
 app = Flask(__name__)
 
-numPosts = 3
+# Number of posts to render
+NUM_POSTS = 3
 
-# intitialize category
-def initCategory(name):
-	return Category(name)
+def init_category(name):
+    '''
+    Creates a new category object which points to the category with
+    the specified name.
+    '''
+    return Category(name)
 
-def initUser(name):
-	return PublicUser(name)
+def init_public_user(name):
+    '''
+    Creates a new public user object which points to the user with
+    the specified name.
+    '''
+    return PublicUser(name)
 
-# get category information:
-def getCategory(cat):
-	reddit = Reddit(cat.subreddit[0], numPosts)
-	urls = reddit.getImageUrl()
-	titles = reddit.getTitle()
-	scores = reddit.getScore()
-	authors = reddit.getAuthor()
-	return [urls, titles, scores, authors]
+def get_category(category):
+    '''
+    Gets the information from the specified public category object.
 
-def getUser(user):
-	print(str(user.returnSubreddit()))
-	reddit = Reddit(user.returnSubreddit()[2], numPosts)
-	urls = reddit.getImageUrl()
-	titles = reddit.getTitle()
-	scores = reddit.getScore()
-	authors = reddit.getAuthor()
-	return [urls, titles, scores, authors]
+    Returns a 2D parallel list: [img_urls, titles, scores, authors]
+    '''
+    
+    # initializes the reddit praw wrapper
+    reddit = Reddit(category.subreddit[0], NUM_POSTS)
 
+    # gets the data from reddit
+    img_urls = reddit.getImageUrl()
+    titles = reddit.getTitle()
+    scores = reddit.getScore()
+    authors = reddit.getAuthor()
 
-# home page
+    return [img_urls, titles, scores, authors]
+
+def get_public_user(user):
+    '''
+    Gets the information from the specified public user object.
+
+    Returns a 2D parallel list: [img_urls, titles, scores, authors]
+    '''
+    # initializes the reddit praw wrapper
+    reddit = Reddit(user.returnSubreddit()[2], NUM_POSTS)
+
+    # gets the data from reddit
+    img_urls = reddit.getImageUrl()
+    titles = reddit.getTitle()
+    scores = reddit.getScore()
+    authors = reddit.getAuthor()
+
+    return [img_urls, titles, scores, authors]
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    '''
+    Renders the index page.
+    '''
+    return render_template('index.html')
 
 # user home page
 @app.route('/users')
-def user():
-	name = request.args.get('user')
-	user = initUser(name)
-	[urls, titles, scores, authors] = getUser(user)
-	return render_template('category_page.html', name=name, img_1_url=urls[0], img_2_url=urls[1], img_3_url=urls[2], source_img="content/reddit_logo.png")
+def public_user_page():
+    '''
+    Renders the user page.
+    '''
+
+    name = request.args.get('user')
+    user = init_public_user(name)
+
+    [img_urls, titles, scores, authors] = get_public_user(user)
+
+    return render_template('category_page.html', name=name,
+    img_1_url=img_urls[0], img_2_url=img_urls[1], img_3_url=img_urls[2],
+    source_img="content/reddit_logo.png")
 
 @app.route('/category')
-def category():
-	name = request.args.get('cat')
-	print("Getting category " + name)
-	category = initCategory(name)
-	print("Getting data")
-	[urls, titles, scores, authors] = getCategory(category)
-	return render_template('category_page.html', name=name, img_1_url=urls[0], img_2_url=urls[1], img_3_url=urls[2], source_img="content/reddit_logo.png")
+def category_page():
+    '''
+    Renders the category page.
+    '''
+
+    name = request.args.get('cat')
+    category = init_category(name)
+    
+    [img_urls, titles, scores, authors] = get_category(category)
+    
+    return render_template('category_page.html', name=name,
+    img_1_url=img_urls[0], img_2_url=img_urls[1], img_3_url=img_urls[2],
+    source_img="content/reddit_logo.png")
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
