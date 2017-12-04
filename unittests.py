d49ff694f5bbfc293e60a3e374f1ffae2e2f4ba4
apa-123 <aparajitha@gmail.com>
2017-12-04 10:38:59 -0600

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -5,13 +5,36 @@ import unittest2
 class TestCategoryMethods(unittest.TestCase):
 
     def test_get_picture(self):
-        self.assertEqual(self.get_picture, )
+        self.assertEqual(self.get_picture(), )
 
     def test_get_subreddit(self):
-        self.assertEqual(self.get_subreddit, )
+        self.assertEqual(self.get_subreddit(), )
 
     def test_get_bio(self):
-        self.assertEqual(self.get_bio, )
+        self.assertEqual(self.get_bio(), )
+
+class TestUserMethods(unittest.TestCase):
+
+    def test_get_first(self):
+        self.assertEqual(self.get_first(), )
+
+    def test_get_second(self):
+        self.assertEqual(self.get_second(), )
+
+    def test_get_bio(self):
+        self.assertEqual(self.get_bio(), )
+
+    def test_get_picture(self):
+        self.assertEqual(self.get_picture(), )
+
+    def test_get_age(self):
+        self.assertEqual(self.get_age(), )
+    
+    def test_get_geography(self):
+        self.assertEqual(self.get_geography(), )
+    
+    def test_get_subreddit(self):
+        self.assertEqual(self.get_subreddit(), )
  
 
 if __name__ == '__main__':
