00d74b0fd85d0c23d5f050fa4f50afbb66fe8db5
Aparajitha Adiraju <aparajitha@gmail.com>
2017-12-03 23:25:24 -0600

    unittests

---
+++ unittests.py
@@ -0,0 +1,18 @@
+import unittest2
+
+#WIP figuring out unit tests
+
+class TestCategoryMethods(unittest.TestCase):
+
+    def test_get_picture(self):
+        self.assertEqual(self.get_picture, )
+
+    def test_get_subreddit(self):
+        self.assertEqual(self.get_subreddit, )
+
+    def test_get_bio(self):
+        self.assertEqual(self.get_bio, )
+ 
+
+if __name__ == '__main__':
+    unittest2.main()
