fcf3047859c0a648e432bbb12165e960c877cfbb
Aparajitha Adiraju <aparajitha@gmail.com>
2017-12-12 17:46:32 -0600

    added comments

--- registration_form.py
+++ registration_form.py
@@ -1,6 +1,7 @@
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 class RegistrationForm(Form):
+    #This is used to create a new user in the database when you first register
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
     password = PasswordField('New Password', [
